fbab8ba9d85c385ca2fe4ec5da409c300df14a51
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-05-04 12:01:22 +0900

    first commit

---
+++ src/main/java/com/example/es_springboot/EsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.es_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/es_springboot/EsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.es_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
