3cd19cca2201881d6ac042a2adbb0fb5c8f8cc83
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2023-06-02 19:52:54 +0900

    feat(complete) : ELK 설명 주석 추가

--- src/test/java/com/example/es_springboot/config/ElasticTest.java
+++ src/test/java/com/example/es_springboot/config/ElasticTest.java
@@ -16,29 +16,31 @@ import org.testcontainers.images.builder.ImageFromDockerfile;
 @EnableElasticsearchRepositories(basePackageClasses = UserSearchRepository.class)
 public class ElasticTest extends AbstractElasticsearchConfiguration {
 
+    // nori를 다운받기 위해서 DockerImageName보 더 추상화된 GenericContainer를 사용합니다.
     private static final GenericContainer container;
 
     static{
         container = new GenericContainer(
                 new ImageFromDockerfile().withDockerfileFromBuilder(
                         b -> {
-                            b.from("docker.elastic.co/elasticsearch/elasticsearch:7.10.2")
-                                    .run("bin/elasticsearch-plugin install analysis-nori")
+                            b.from("docker.elastic.co/elasticsearch/elasticsearch:7.10.2") // 엘라스틱 이미지입니다.
+                                    .run("bin/elasticsearch-plugin install analysis-nori") // 이미지에 nori를 다운 받습니다.
                                     .build();
                         })
         ).withExposedPorts(9200, 9300)
                 .withFileSystemBind(
                         "es/settings/stop/english.txt",
-                        "/usr/share/elasticsearch/config/settings/stop/english.txt",
+                        "/usr/share/elasticsearch/config/settings/stop/english.txt", // 불용어 파일을 추가합니다.
                         BindMode.READ_ONLY)
                 .withFileSystemBind(
                         "es/settings/synonym/english.txt",
-                        "/usr/share/elasticsearch/config/settings/synonym/english.txt",
+                        "/usr/share/elasticsearch/config/settings/synonym/english.txt", // 동의어 파일을 추가합니다.
                         BindMode.READ_ONLY)
                 .withEnv("discovery.type","single-node");
         container.start();
     }
 
+    //엘라스틱서치를 사용하기 위해서는 high와 low level을 사용하는데, 주로 high level을 사용합니다.
     @Override
     public RestHighLevelClient elasticsearchClient() {
         String hostAddress = new StringBuilder()
